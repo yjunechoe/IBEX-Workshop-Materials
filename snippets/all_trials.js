ea9a3dbf85bef051347dc0647e479febc73e7747
June Choe <jchoe001@gmail.com>
2020-12-31 01:02:33 +0900

    Putting together the trials

---
+++ snippets/all_trials.js
@@ -0,0 +1,25 @@
+var item = [
+
+  //// Practice
+  ["practice-1", "AcceptabilityJudgment", {s: "The car drove like a dream."}],
+
+  // Critical Trials //
+
+  //// Set #1
+  [["gp.trans",1], "AcceptabilityJudgment", {s: "While Anna trained the kitten paid attention."}],
+  [["gp.intrans",1], "AcceptabilityJudgment", {s: "While Anna dressed the kitten paid attention."}],
+
+  //// Set #2
+  [["gp.trans",2], "AcceptabilityJudgment", {s: "Since Dave improved the department was satisfied."}],
+  [["gp.intrans",2], "AcceptabilityJudgment", {s: "Since Dave worried the counselor devised a plan."}]
+
+  //// Fillers (Good)
+  ["filler-good-01","AcceptabilityJudgment", {s: "When Harry fell, the audience was shocked."}],
+
+  //// Fillers (Bad)
+  ["filler-bad-01","AcceptabilityJudgment", {s: "When Tyler sneezed the driver, he passed a tissue."}],
+
+  //// Fillers (Catch)
+  ["filler-catch-01", "AcceptabilityJudgment", {s: "Please select 4 for this sentence."}]
+
+]

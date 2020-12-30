aae608623fdbbf7130b542c2faf28bb92bc133b8
June Choe <jchoe001@gmail.com>
2020-12-30 23:06:12 +0900

    putting it together

---
+++ snippets/critical_trials.js
@@ -0,0 +1,9 @@
+var item = [
+  //// Set #1
+  [["gp.trans",1], "AcceptabilityJudgment", {s: "While Anna trained the kitten paid attention."}],
+  [["gp.intrans",1], "AcceptabilityJudgment", {s: "While Anna dressed the kitten paid attention."}],
+
+  //// Set #2
+  [["gp.trans",2], "AcceptabilityJudgment", {s: "Since Dave improved the department was satisfied."}],
+  [["gp.intrans",2], "AcceptabilityJudgment", {s: "Since Dave worried the counselor devised a plan."}]
+]

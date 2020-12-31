3ba2670c08e37dcdbd741812c0503aa49763229b
June Choe <jchoe001@gmail.com>
2020-12-31 16:23:35 +0900

    minimal experiment

--- ibex_files/minimal_exp.js
+++ ibex_files/minimal_exp.js
@@ -1,11 +1,35 @@
+// Options and Other Variables //
+
+//// Generates random number assigned to participants (Participant ID)
+var randomCode = Math.random().toString(36).substr(2,9)
+//// A message to show to participants at completion (useful for confirmation, raffle entry, etc.)
+var completionMessage = "Thank you for your participation. Your participation code is: " + randomCode
+
+//// Show a progress bar at the top? (true/false)
+var showProgressBar = false
+
+//// Override default settings for controllers (parameters go inside the curly braces { })
+var defaults = [
+    "AcceptabilityJudgment", {
+       as: ["1", "2", "3", "4", "5", "6", "7"],           
+       presentAsScale: true,                             
+       instructions: "Use number keys or click boxes to answer.",    
+       leftComment: "(Bad)",
+       rightComment: "(Good)"
+    }]
+
+
 // Presentation Order //
+
 var shuffleSequence = seq(
   "setcounter",
   startsWith("practice"),
   rshuffle(startsWith("gp"), startsWith("filler"))
 )
 
+
 // Experiment Materials //
+
 var items = [
 
   //// Counter
--- snippets/settings.js
+++ snippets/settings.js
@@ -1,18 +1,14 @@
-// Definitions of main variables //
+// Options and Other Variables //
 
 //// Generates random number assigned to participants (Participant ID)
-var randomCode = Math.random().toString(36).substr(2,9);
-//// A message to show to participants at completion (useful for verification, raffle entry, etc.)
-var completionMessage = "Thank you for your participation. The results were successfully transmitted. Your participation code is: " + randomCode;
+var randomCode = Math.random().toString(36).substr(2,9)
 
-//// Show a progress bar at the top? (true/false)
-var showProgressBar = false;
-
-//// Override default settings for a controller (parameters go inside the curly braces { })
-var defaults = ["AcceptabilityJudgment", {
+//// Override default settings for controllers (parameters go inside the curly braces { })
+var defaults = [
+    "AcceptabilityJudgment", {
        as: ["1", "2", "3", "4", "5", "6", "7"],           
        presentAsScale: true,                             
        instructions: "Use number keys or click boxes to answer.",    
        leftComment: "(Bad)",
        rightComment: "(Good)"
-    }];
+    }]

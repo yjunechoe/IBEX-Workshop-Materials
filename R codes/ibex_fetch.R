53d9fabbbfaf60891a71d03de12e5d2016266f0f
June Choe <jchoe001@gmail.com>
2021-01-04 14:25:56 +0900

    minor code changes

--- R codes/ibex_fetch.R
+++ R codes/ibex_fetch.R
@@ -1,17 +1,23 @@
-library(dplyr)
-library(rvest)
-library(RSelenium)
-
-
-#### Edit details here
+############# Edit details here #############
 USERNAME <- "skku_ibex"
 PASSWORD <- "2021workshop"
 EXPERIMENT <- "workshop_DashedSentence"
-###
+RESULTS_FILENAME <- "results.csv"
+#############################################
 
+
+## Setup ##
+
+library(dplyr)
+library(rvest)
+library(RSelenium)
+source('R codes/read_ibex.R')
+
+
+## Connect to IBEX and get results ##
 
 rD <- rsDriver(
-  browser = "firefox", port = 2021L, verbose = F,
+  browser = "firefox", port = as.integer(runif(1, 1001, 9999)), verbose = F,
   extraCapabilities = list(
     "moz:firefoxOptions" = list(
       args = list('--headless')
@@ -36,24 +42,20 @@ experiments <- read_html(html) %>%
   html_nodes("a") %>% 
   html_text()
 
-experiment <- remDr$findElement(using = "link text", value = experiments[1])
+message("Present experiments:\n", paste0(paste("-", experiments), collapse = "\n"))
+message("Selected experiment:\n", paste("-", EXPERIMENT))
 
-experiment$clickElement()
-
-Sys.sleep(1)
+if (!EXPERIMENT %in% experiments) stop("Invalid experiment name. Please choose among present experiments.")
 
 remDr$navigate(paste0("https://spellout.net/ibexfarm/ajax/download/", EXPERIMENT, "/results/results"))
 
-experiment_results <- remDr$findElement(using = "tag", value = "pre")$getElementText()[[1]] %>% 
-  stringr::str_split('\n') %>% 
-  `[[`(1)
 
-experiment_cols <- purrr::discard(stringr::str_extract(experiment_results, "(?<=^# \\d{1,2}\\. ).*(?=(\\.|\\?)$)"), is.na)
+## Process results file and save ##
 
-experiment_data <- purrr::discard(experiment_results, ~ stringr::str_detect(.x, "^#"))
-
-results <- readr::read_csv(experiment_data, col_names = unique(experiment_cols))
+raw_results <- remDr$findElement(using = "tag", value = "pre")$getElementText()[[1]]
 
 invisible(remDr$closeall())
 
-readr::write_csv(results, "results.csv")
+results <- read_ibex(strsplit(raw_results, "\n")[[1]], external = FALSE)
+
+write.csv(results, RESULTS_FILENAME)
